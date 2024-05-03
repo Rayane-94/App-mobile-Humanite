5035f06203fe60eedd67377905c6e70fd0fb93a7
Rayane Besrour <rayanebesrour3@gmail.com>
2024-05-03 22:59:25 +0200

    Mise au point du style 03/05

--- screens/Scan.js
+++ screens/Scan.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Text, View, Image, StyleSheet } from 'react-native';
+import { Button, Text, View,StyleSheet } from 'react-native';
 import { Camera } from 'expo-camera';
 import { useNavigation } from '@react-navigation/native';
 
 export default function Scan() {
-  
   const navigation = useNavigation();
-
   const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
+  const [scanned, setScanned] = useState(false); // Ajout de l'état scanner
 
   useEffect(() => {
     (async () => {
@@ -19,30 +17,39 @@ export default function Scan() {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    alert(`Le type de QRCode est ${type} et son content ${data} a eter scanner!`);
+  };
+
+  const handleScanAgain = () => { // Fonction pour gérer le scan à nouveau
+    setScanned(false);
   };
 
   if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
+    return <Text>Autoriser vous que nous utulisons votre camera</Text>;
   }
   if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+    return <Text>Nous ne pouvons acceder a votre camera</Text>;
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}> {'Ramasse'.toUpperCase()} </Text>
       <Text style={styles.subtitle}> {'Blue Project'.toUpperCase()} </Text>
-      <Button
-        title='Retour'
-        onPress={() => navigation.goBack()} color='#7ed957' />
-
       <Camera
         style={styles.camera}
         type={Camera.Constants.Type.back}
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} // Ignorer le scan si déjà scanné
       />
-      {scanned && <Text style={styles.text}>Tap to Scan Again</Text>}
+      <Button style={styles.boutonRetour}
+        title='Retour'
+        onPress={() => navigation.goBack()} color='#7ed957' />
+
+      
+      {scanned && (
+        <View style={styles.buttonNouveau}>
+          <Button title="Nouveau Qr code" onPress={handleScanAgain} color="#7ed957" />
+        </View>
+      )}
     </View>
   );
 }
@@ -61,14 +68,22 @@ const styles = StyleSheet.create({
   subtitle: {
     fontSize: 40,
     color: '#629AE5',
+    marginBottom:50,
   },
+  boutonRetour: {
+    marginBottom:50,
+  },
+ 
   camera: {
-    flex: 1,
-    width: '100%',
+    marginBottom:50,
+    width: '60%',
+    height: '40%',
   },
   text: {
     fontSize: 20,
     color: 'white',
     marginTop: 20,
   },
+  
+ 
 });
